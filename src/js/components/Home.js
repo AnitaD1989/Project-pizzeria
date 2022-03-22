5b73cc6a2f2475e2f5b7f13dd9f5205b9080c6d7
Anita Deszczynska <anitadeszczynska@gmail.com>
2022-03-22 10:42:27 +0100

    Add initLinks and redirection while clicking on boxes in Home page

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -36,6 +36,41 @@ class Home {
 
     thisHome.pages = document.querySelector(select.containerOf.pages).children;
     thisHome.navLinks = document.querySelectorAll(select.nav.links);
+
+    for(let page of thisHome.pages){
+
+      if(page.id == pageId){
+        page.classList.add(classNames.pages.active);
+      
+      } else {
+        page.classList.remove(classNames.pages.active);
+      }
+    }
+
+    for(let link of thisHome.navLinks){
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  }
+
+  initLinks(){
+    const thisHome = this;
+
+    thisHome.boxLinks = document.querySelectorAll('.home-link');
+
+    for(let link of thisHome.boxLinks){
+      link.addEventListener('click', function(event){
+        event.preventDefault();
+
+        const clickedLink = this;
+
+        const id = clickedLink.getAttribute('href').replace('#', '');
+
+        thisHome.activatePage(id);
+      });
+    }
   }
 }
 
